682770b2b351a2cf5300e706c706c7f0e7a507e1
fishboy1900 <49224730+fishboy1900@users.noreply.github.com>
2020-10-03 12:02:55 -0300

    Update hash_game.py

--- hash_game.py
+++ hash_game.py
@@ -13,13 +13,34 @@ else:
 
 X = [[0,0,0],[0,0,0],[0,0,0]]
 for i in range(9):
+      print(X)
       j_1 = input('Jogador 1 (x,y): ')
       try:
             if len(j_1) == 3 and int(j_1[0]) >= 0 and int(j_1[2]) <= 2:
-                  print("Chegou aqui 1")
-                  X[int(j_1[0])][int(j_1[2])] = 1
-                  print(X)
-            
+                  if X[int(j_1[0])][int(j_1[2])] == 0:
+                        X[int(j_1[0])][int(j_1[2])] = 1
+                        print(X)
+                  
+                  else:
+                        raise InvalidPlay
+
+            else:
+                  raise InvalidInput 
+
+      except:
+            raise InvalidInput
+
+      j_2 = input('Jogador 2 (x,y): ')
+
+      try:
+            if len(j_2) == 3 and int(j_2[0]) >= 0 and int(j_2[2]) <= 2:
+                  if X[int(j_2[0])][int(j_2[2])] == 0:
+                        X[int(j_2[0])][int(j_2[2])] = 2
+                        print(X, "\n",'=================================')
+                  
+                  else:
+                        raise InvalidPlay
+
             else:
                   raise InvalidInput 
 
