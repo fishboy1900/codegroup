6f0f8742e97bc4d50711eec07bfb410f7190bc28
fishboy1900 <49224730+fishboy1900@users.noreply.github.com>
2020-10-03 13:42:04 -0300

    Update hash_game.py

--- hash_game.py
+++ hash_game.py
@@ -5,14 +5,14 @@ class InvalidPlay(Exception):
 	pass
 
 def winner(X):
-      win = ''
+      win = 'N'
       # Horizontal case.
       for l in range(3):
             if X[l][0] == X[l][1] == X[l][2] == 1:
-                  win = 'Vencedor: Jogador 1'
+                  win = 'V1'
 
             elif X[l][0] == X[l][1] == X[l][2] == 2:
-                  win = 'Vencedor: Jogador 2'
+                  win = 'V2'
 
             else:
                   pass
@@ -20,20 +20,26 @@ def winner(X):
       # Vertical case.
       for c in range(3):
             if X[0][c] == X[1][c] == X[2][c] == 1:
-                  win = 'Vencedor: Jogador 1'
+                  win = 'V1'
 
             elif X[0][c] == X[1][c] == X[2][c] == 2:
-                  win = 'Vencedor: Jogador 2'
+                  win = 'V2'
 
             else:
                   pass
 
-      # Diagonal case.
+      # Diagonal cases.
       if X[0][0] == X[1][1] == X[2][2] == 1:
-            win = 'Vencedor: Jogador 1'
+            win = 'V1'
 
       elif X[0][0] == X[1][1] == X[2][2] == 2:
-            win = 'Vencedor: Jogador 2'
+            win = 'V2'
+
+      elif X[2][0] == X[1][1] == X[0][2] == 1:
+            win = 'V1'
+
+      elif X[2][0] == X[1][1] == X[0][2] == 2:
+            win = 'V2'
 
       else:
             pass
@@ -43,9 +49,10 @@ def winner(X):
 def main():
 
       X = [[0,0,0],[0,0,0],[0,0,0]]
-      print(winner(X))
-      for i in range(9):
-            print(X)
+      p = 0
+      while p<=8:
+            for line in X:
+                  print(line)
             j_1 = input('Jogador 1 (x,y): ')
             try:
                   # Validating the input.
@@ -54,13 +61,20 @@ def main():
                         if X[int(j_1[0])][int(j_1[2])] == 0:
                               # Marking the place for the player 1.
                               X[int(j_1[0])][int(j_1[2])] = 1
-                              print(X)
-                              # Checking if there is a winner.
-                              if winner(X)[:8] == 'Vencedor':
-                                    print(winner(X))
+                              p += 1
+                              print("p: ",p)
+                              for line in X:
+                                    print(line)
+                              if p == 9:
+                                    print("Empate")
                                     break
-                              else: 
-                                    pass
+                              else:
+                                    # Checking if there is a winner.
+                                    if winner(X)[0] == 'V':
+                                          print(winner(X)[1])
+                                          break
+                                    else: 
+                                          pass
                         
                         else:
                               raise InvalidPlay
@@ -70,7 +84,7 @@ def main():
 
             except:
                   raise InvalidInput
-
+                  
             # Now, its second's player time.
             j_2 = input('Jogador 2 (x,y): ')
 
@@ -81,14 +95,17 @@ def main():
                         if X[int(j_2[0])][int(j_2[2])] == 0:
                               # Marking the place for the player 2.
                               X[int(j_2[0])][int(j_2[2])] = 2
-                              print(X)
+                              p += 1
+                              print("p: ",p)
+                              for line in X:
+                                    print(line)
                               # Checking if there is a winner.
-                              if winner(X)[:8] == 'Vencedor':
-                                    print(winner(X))
+                              if winner(X)[0] == 'V':
+                                    print(winner(X)[1])
                                     break
                               else: 
                                     pass
-                              print("\n", '=================================')
+                              print("\n", '=================================', "\n")
                         
                         else:
                               raise InvalidPlay
