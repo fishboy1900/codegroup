1dddbcd0925d4f20adae8daf91a8da290e64a137
fishboy1900 <49224730+fishboy1900@users.noreply.github.com>
2020-10-03 11:52:29 -0300

    Update hash_game.py

--- hash_game.py
+++ hash_game.py
@@ -1,1 +1,28 @@
-print('Hello World!')
+class InvalidInput(Exception):
+	pass
+
+class InvalidPlay(Exception):
+	pass
+
+'''j_1 = input("Coordenadas: ")
+if len(j_1) == 3 and int(j_1[0]) >= 0 and int(j_1[2]) <= 2:
+      print("OK")
+      print(int(j_1[0]))
+else:
+      print("bosta")'''
+
+X = [[0,0,0],[0,0,0],[0,0,0]]
+for i in range(9):
+      j_1 = input('Jogador 1 (x,y): ')
+      try:
+            if len(j_1) == 3 and int(j_1[0]) >= 0 and int(j_1[2]) <= 2:
+                  print("Chegou aqui 1")
+                  X[int(j_1[0])][int(j_1[2])] = 1
+                  print(X)
+            
+            else:
+                  raise InvalidInput 
+
+      except:
+            raise InvalidInput
+      
