fe16d23b1f09ced5a85cf1a7efb3c776189f17b4
Thiago Bibiano <thiagobibiano@ymail.com>
2020-10-04 20:57:38 -0300

    Refatoramento do jogo da velha, enxugando-o e buscando melhor entendimento ao mesmo.

--- hash_game.py
+++ hash_game.py
@@ -4,115 +4,71 @@ class InvalidInput(Exception):
 class InvalidPlay(Exception):
 	pass
 
+def hash_board(X):
+    
+    tabuleiro =  '''
+{} | {} | {}
+------------
+{} | {} | {}
+------------
+{} | {} | {}
+'''.format(*X[0], *X[1], *X[2])
+
+    return tabuleiro
+    
 def winner(X):
-      win = 'N'
-      # Horizontal case.
-      for l in range(3):
-            if X[l][0] == X[l][1] == X[l][2] == 1:
-                  win = 'V1'
+  # Horizontal case.
+    for l in range(3):
+        if X[l][0] == X[l][1] == X[l][2] != 0:
+            return True
 
-            elif X[l][0] == X[l][1] == X[l][2] == 2:
-                  win = 'V2'
+  # Vertical case.
+    for c in range(3):
+        if X[0][c] == X[1][c] == X[2][c] != 0:
+            return True
 
-            else:
-                  pass
+  # Diagonal cases.
+    if X[0][0] == X[1][1] == X[2][2] != 0:
+        return True
 
-      # Vertical case.
-      for c in range(3):
-            if X[0][c] == X[1][c] == X[2][c] == 1:
-                  win = 'V1'
+    elif X[2][0] == X[1][1] == X[0][2] != 0:
+        return True
 
-            elif X[0][c] == X[1][c] == X[2][c] == 2:
-                  win = 'V2'
-
-            else:
-                  pass
-
-      # Diagonal cases.
-      if X[0][0] == X[1][1] == X[2][2] == 1:
-            win = 'V1'
-
-      elif X[0][0] == X[1][1] == X[2][2] == 2:
-            win = 'V2'
-
-      elif X[2][0] == X[1][1] == X[0][2] == 1:
-            win = 'V1'
-
-      elif X[2][0] == X[1][1] == X[0][2] == 2:
-            win = 'V2'
-
-      else:
-            pass
-
-      return win
 
 def main():
 
-      X = [[0,0,0],[0,0,0],[0,0,0]]
-      p = 0
-      while p<=8:
-            for line in X:
-                  print(line)
-            j_1 = input('Jogador 1 (x,y): ')
-            try:
-                  # Validating the input.
-                  if len(j_1) == 3 and int(j_1[0]) >= 0 and int(j_1[2]) <= 2:
-                        # Checking if the place is already filled.
-                        if X[int(j_1[0])][int(j_1[2])] == 0:
-                              # Marking the place for the player 1.
-                              X[int(j_1[0])][int(j_1[2])] = 1
-                              p += 1
-                              print("p: ",p)
-                              for line in X:
-                                    print(line)
-                              if p == 9:
-                                    print("Empate")
-                                    break
-                              else:
-                                    # Checking if there is a winner.
-                                    if winner(X)[0] == 'V':
-                                          print(winner(X)[1])
-                                          break
-                                    else: 
-                                          pass
-                        
-                        else:
-                              raise InvalidPlay
-
-                  else:
-                        raise InvalidInput 
-
-            except:
-                  raise InvalidInput
-                  
-            # Now, its second's player time.
-            j_2 = input('Jogador 2 (x,y): ')
+    X = [[0,0,0],[0,0,0],[0,0,0]]
+    valid_input = (0, 1, 2)
+    
+    # infinite loop
+    while True:
+        # looping to change player
+        for player in (1,2):
+            # printing the hash board
+            print(hash_board(X))
+            
+            move = input('Por favor Jogador {}, faça sua jogada (x,y): '.format(player))
+            # built in function to cut the input in two pair tuple
+            move_tuple = move.split(",")
+            move_tuple = [int(num) for num in move_tuple] # compact for to change strings to in
 
             try:
-                  # Validating the input.
-                  if len(j_2) == 3 and int(j_2[0]) >= 0 and int(j_2[2]) <= 2:
-                        # Checking if the place is already filled.
-                        if X[int(j_2[0])][int(j_2[2])] == 0:
-                              # Marking the place for the player 2.
-                              X[int(j_2[0])][int(j_2[2])] = 2
-                              p += 1
-                              print("p: ",p)
-                              for line in X:
-                                    print(line)
-                              # Checking if there is a winner.
-                              if winner(X)[0] == 'V':
-                                    print(winner(X)[1])
-                                    break
-                              else: 
-                                    pass
-                              print("\n", '=================================', "\n")
-                        
-                        else:
-                              raise InvalidPlay
-
-                  else:
-                        raise InvalidInput 
+            # Validating the input.
+                if move_tuple[0] in valid_input and move_tuple[1] in valid_input and X[move_tuple[0]][move_tuple[1]] == 0:
+                    # Marking the place for the player 1 or 2.
+                    X[move_tuple[0]][move_tuple[1]] = player
+                    if winner(X):
+                        print("Parabéns, o Jogador {} ganhou!!!".format(player))
+                        return
+                    
+                    for line in X:
+                        if 0 not in line:
+                            print("Empate!")
+                            return
+                    else:
+                        continue
 
             except:
-                  raise InvalidInput
+                raise InvalidInput
+
 main()
