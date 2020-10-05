c3658c08e99cca0cf732028ffef93750143d2b03
fishboy1900 <49224730+fishboy1900@users.noreply.github.com>
2020-10-05 12:19:56 -0300

    Update hash_game_random.py

--- hash_game_random.py
+++ hash_game_random.py
@@ -114,9 +114,9 @@ def main():
       df = df.append({'(0,0)': X[0][0],'(0,1)': X[0][1],'(0,2)': X[0][2],'(1,0)': X[1][0],'(1,1)': X[1][1],'(1,2)': X[1][2],'(2,0)': X[2][0],'(2,1)': X[2][1],'(2,2)': X[2][2], 'Winner': winner(X)[1]}, ignore_index=True)
 
 j = 0
-for i in range(5000):
+for i in range(100):
       main()
       j = j + 1
-      print(j)
+      print((j/100)*100,"%")
 
-df.to_csv('result.csv')
+print(df)
