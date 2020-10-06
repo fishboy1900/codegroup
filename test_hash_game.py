33b209b85a1830968393ba98557a4197500f68df
Thiago Bibiano <thiagobibiano@ymail.com>
2020-10-06 18:41:10 -0300

    Refatorando testes para winner

--- test_hash_game.py
+++ test_hash_game.py
@@ -2,22 +2,21 @@ from hash_game import main, winner, valid_move, hash_board
 import pytest
 
 
-def test_winner():
-    tabs =( 
-        ([[1,1,1],[0,2,0],[0,0,2]], True),
-        ([[1,0,0],[1,0,2],[1,2,0]], True),
-        ([[1,2,0],[2,1,0],[0,0,1]], True),
-        ([[0,2,1],[0,1,2],[1,0,0]], True),
-        ([[1,2,2],[1,2,2],[2,1,0]], True),
-        ([[0,0,0],[0,0,0],[0,0,0]], False),
-        ([[0,1,0],[2,0,2],[0,1,0]], False),
-        ([[1,0,0],[0,2,0],[0,0,1]], False),
-        ([[1,2,1],[1,2,2],[2,1,1]], False),
-        ([[1,2,2],[2,1,1],[1,1,2]], False)
-        )
+@pytest.fixture( params=[
+    {"input": [[1,1,1],[0,2,0],[0,0,2]], "expectedResult" : True},
+    {"input": [[1,0,0],[1,0,2],[1,2,0]], "expectedResult" : True},
+    {"input": [[1,2,0],[2,1,0],[0,0,1]], "expectedResult" : True},
+    {"input": [[0,2,1],[0,1,2],[1,0,0]], "expectedResult" : True},
+    {"input": [[1,2,2],[1,2,2],[2,1,0]], "expectedResult" : True},
+    {"input": [[0,0,0],[0,0,0],[0,0,0]], "expectedResult" : False},
+    {"input": [[0,1,0],[2,0,2],[0,1,0]], "expectedResult" : False},
+    {"input": [[1,0,0],[0,2,0],[0,0,1]], "expectedResult" : False},
+    {"input": [[1,2,1],[1,2,2],[2,1,1]], "expectedResult" : False},
+    {"input": [[1,2,2],[2,1,1],[1,1,2]], "expectedResult" : False}])
 
-    for (tab, end) in tabs:
+def testCase(request):
+    return request.param
 
-        assert winner(tab) == end
-
-test_winner()
+def test_winner(testCase):
+    result = winner(testCase["input"])
+    assert result == testCase["expectedResult"]
